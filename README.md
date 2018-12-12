35def4020713d59c85e1cd834ef489934a61595f
Aldifashatv <aldifashatv@gmail.com>
2018-12-12 15:15:17 +0700

    this is third commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# project1
+# this is a sample README.md file.
+#it contains the project1 description.
---
+++ sample.java
@@ -0,0 +1,10 @@
+/* Here code starts */
+public class Sample(
+	public static void main (String args[]){
+		system.out.println("This is just a sample java file for Git");
+		system.out.println("We are adding the output lines to see the changes.");
+		system.out.println("This is second modification in java file to see the staged and unstaged changes.");
+		}
+			}
+
+	/* Here code ends */
